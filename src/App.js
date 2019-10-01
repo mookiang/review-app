b2353e370a0655bc9b1779c66f4e728c013bb91c
Mookiang <mookiang@gmail.com>
2019-10-01 23:03:47 +0900

    # add add: Drama component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types";
+
+function Drama({name, actor, rating}) {  
+  return (
+    <div>
+      <h1>I Like {name} by {actor}</h1>
+      <h3>{rating}</h3>
+    </div>
+  );
+}
+
+Drama.propTypes = {
+  name: PropTypes.string.isRequired,
+  actor: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+const dramaILike = [
+  {
+    id: 1,
+    name: "My Mister",
+    actor: "IU",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Woods of Secret",
+    actor: "Cho SeungWoo",
+    rating: 4.8,
+  },
+  {
+    id: 3,
+    name: "Dokkaebi",
+    actor: "Kim GoEun"    ,
+    rating: 4.5,
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>      
+      {dramaILike.map(drama => (
+        <Drama  key={drama.id} name={drama.name} actor={drama.actor} 
+          rating={drama.rating} />
+      ))}
     </div>
   );
 }
