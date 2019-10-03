742c3aa593b02b0caf579d3d8a425612f9764f90
Mookiang <mookiang@gmail.com>
2019-10-03 23:01:22 +0900

    Updates

---
+++ precache-manifest.67c2cab3239304df79745410d130b1ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "573a85a3a2a82d1eb79faae7da86c150",
+    "url": "/review-app/index.html"
+  },
+  {
+    "revision": "bbe0bd68f1d7a32544f5",
+    "url": "/review-app/static/css/main.f8b5f959.chunk.css"
+  },
+  {
+    "revision": "c6f1b1de03955ddc914c",
+    "url": "/review-app/static/js/2.cdcfcbb4.chunk.js"
+  },
+  {
+    "revision": "bbe0bd68f1d7a32544f5",
+    "url": "/review-app/static/js/main.c44ee45a.chunk.js"
+  },
+  {
+    "revision": "d75652a21bf4a210f953",
+    "url": "/review-app/static/js/runtime-main.a12f1f53.js"
+  }
+]);
